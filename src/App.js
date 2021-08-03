13f83d7275a40c03546ca84151817469c049065e
Axel <axosar@gmail.com>
2021-08-03 18:11:14 -0500

    env

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
 
 import './App.css';
-
-axios.defaults.xsrfCookieName = 'CSRF-TOKEN';
-axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
-
 function App() {
-  const { register, handleSubmit, errors, formState } = useForm();
-
-  async function onSubmit(data) {
-    try {
-      const response = await axios.post('/api/books', data);
-      console.log(response);
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   return (
     <div>
-      <h1>Save a new book</h1>
-
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            name="title"
-            placeholder="Title"
-            ref={register({
-              required: 'Required',
-            })}
-          />
-          <label htmlFor="title">{errors.title && errors.title.message}</label>
-
-          <input name="subtitle" placeholder="Subtitle" ref={register()} />
-          <label htmlFor="subtitle">
-            {errors.subtitle && errors.subtitle.message}
-          </label>
-
-          <button type="submit" disabled={formState.isSubmitting}>
-            Save
-          </button>
-        </form>
-      </div>
+      <h1>CHANNEL PROGRAM</h1>
     </div>
   );
 }
