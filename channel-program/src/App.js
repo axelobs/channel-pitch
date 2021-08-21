382a5553fc0d83fdb144f67320eb68123f10bd14
Pedro Mendes <hello@pedrofront.dev>
2021-08-20 21:00:33 -0300

    feat: adding about us page

--- channel-program/src/App.js
+++ channel-program/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/common/Footer';
 import Register from './components/User/Register';
 import Events from './components/Events';
 import Subscription from './components/Subscription';
+import AboutUs from './components/AboutUs';
 
 import {
   BrowserRouter as Router,
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/register" component={Register} />
           <Route exact path="/events" component={Events} />
           <Route exact path="/subscribe" component={Subscription} />
+          <Route exact path="/about-us" component={AboutUs} />
           <Route path="/" />
         </Switch>
         <Footer />
